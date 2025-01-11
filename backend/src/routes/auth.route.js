9ac467f841f58a166a37331cca3ec1e87b13e820
Suhani Jain <jainsuhani132@gmail.com>
2025-01-11 16:13:55 +0530

    completed authentication routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express"
-import { login, logout, signup} from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile} from "../controllers/auth.controller.js";
+import {protectRoute} from "../middleware/auth.middleware.js";
+
 
 const router = express.Router();
 
@@ -9,4 +11,8 @@ router.post("/login",login);
 
 router.post("/logout",logout);
 
-export default router
+router.put("/update-profile",protectRoute,updateProfile);
+
+router.get("/check", protectRoute, checkAuth)
+
+export default router;
